59d5d3703d4b9aa4462d1079abefc9d8bbd25b6e
asolovov <andre.solovov@gmail.com>
2025-06-07 17:37:47 +0300

    feat: templates

---
+++ templates/templates.go
@@ -0,0 +1,34 @@
+package templates
+
+import (
+	"embed"
+	"text/template"
+)
+
+//go:embed config/*.gotmpl
+//go:embed internal/*.gotmpl
+//go:embed internal/service/*.gotmpl
+//go:embed internal/repository/*.gotmpl
+//go:embed internal/server/*.gotmpl
+//go:embed internal/server/grpcServer/*.gotmpl
+//go:embed cmd/*.gotmpl
+//go:embed cmd/root/*.gotmpl
+//go:embed cmd/serve/*.gotmpl
+//go:embed models/*.gotmpl
+var templatesFS embed.FS
+
+var Tmpls *template.Template
+
+func init() {
+	var err error
+	Tmpls, err = template.ParseFS(templatesFS,
+		"config/*.gotmpl",
+		"internal/*.gotmpl", "internal/service/*.gotmpl", "internal/repository/*.gotmpl",
+		"internal/server/*.gotmpl", "internal/server/grpcServer/*.gotmpl",
+		"cmd/root/*.gotmpl", "cmd/serve/*.gotmpl", "cmd/*.gotmpl",
+		"models/*.gotmpl",
+	)
+	if err != nil {
+		panic(err)
+	}
+}
