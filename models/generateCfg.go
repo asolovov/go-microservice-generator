fbf2b2a1cdb74b312c69182c9b1a9d0f3a4a4906
asolovov <andre.solovov@gmail.com>
2025-06-07 17:39:01 +0300

    feat: generate config

---
+++ models/generateCfg.go
@@ -0,0 +1,74 @@
+package models
+
+type GenerateCfg struct {
+	Root          string        `yaml:"root" json:"Root"`
+	GoPackage     string        `yaml:"go-package" json:"GoPackage"`
+	GoModGenerate bool          `yaml:"go-mod-generate" json:"GoModGenerate"`
+	Service       ServiceCfg    `yaml:"service" json:"Service"`
+	IsServer      bool          `yaml:"-" json:"IsServer"`
+	GrpcServer    GrpcServerCfg `yaml:"grpc-server" json:"GrpcServer"`
+	Repository    RepositoryCfg `yaml:"repository" json:"Repository"`
+	IsGenModels   bool          `yaml:"-" json:"IsGenModels"`
+	Models        ModelsCfg     `yaml:"models" json:"Models"`
+	Cfg           CfgCfg        `yaml:"cfg" json:"Cfg"`
+	App           AppCfg        `yaml:"app" json:"App"`
+	Cmd           CmdCfg        `yaml:"cmd" json:"Cmd"`
+}
+
+type GrpcServerCfg struct {
+	Generate     bool         `yaml:"generate" json:"-"`
+	DefaultAddr  string       `yaml:"default-addr" json:"DefaultAddr"`
+	ProtoService ProtoService `yaml:"proto-service" json:"ProtoService"`
+	ProtoModels  ProtoModels  `yaml:"proto-models" json:"ProtoModels"`
+}
+
+type ServiceCfg struct {
+	Generate bool `yaml:"generate" json:"-"`
+}
+
+type CfgCfg struct {
+	Generate bool `yaml:"generate" json:"-"`
+}
+
+type AppCfg struct {
+	Generate bool `yaml:"generate" json:"-"`
+}
+
+type CmdCfg struct {
+	Generate bool `yaml:"generate" json:"-"`
+}
+
+type ProtoService struct {
+	Path   string `yaml:"path" json:"Path"`
+	PbName string `yaml:"pb-name" json:"PbName"`
+	GoName string `yaml:"go-name" json:"GoName"`
+}
+
+type ProtoModels struct {
+	Path          string `yaml:"path" json:"Path"`
+	SameAsService bool   `yaml:"-" json:"SameAsService"`
+	GoName        string `yaml:"go-name" json:"GoName"`
+}
+
+type RepositoryCfg struct {
+	Generate       bool   `yaml:"generate" json:"-"`
+	DefaultAddr    string `yaml:"default-addr" json:"DefaultAddr"`
+	MigrationsPath string `yaml:"migrations-path" json:"MigrationsPath"`
+}
+
+type ModelsCfg struct {
+	Generate bool        `yaml:"generate" json:"-"`
+	Path     string      `yaml:"path" json:"Path"`
+	AI       ModelsAICfg `yaml:"ai" json:"AI"`
+}
+
+type ModelsAICfg struct {
+	Generate         bool               `yaml:"generate" json:"-"`
+	ParsingRules     ModelsParsingRules `yaml:"parsing-rules" json:"ParsingRules"`
+	AdditionalPrompt string             `yaml:"additional-prompt" json:"AdditionalPrompt"`
+}
+
+type ModelsParsingRules struct {
+	ProtoDomain map[string]string `yaml:"proto-domain" json:"ProtoDomain"`
+	DomainDB    map[string]string `yaml:"domain-db" json:"DomainDB"`
+}
